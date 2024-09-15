473cb973fccb19ee8752275dc5df33578d429bdb
nnsang1309 <nguyennhatsang2002@gmail.com>
2024-09-15 19:28:45 +0700

    home screen, listtile songs

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:music_app/data/repository/repository.dart';
+import 'package:music_app/ui/home/home.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  var repository = DefaultRepository();
-
-  var songs = await repository.loadData();
-  if (songs != null) {
-    for (var song in songs) {
-      debugPrint(song.toString());
-    }
-  }
-}
-
-class MusicApp extends StatelessWidget {
-  const MusicApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+void main() => runApp(MusicApp());
